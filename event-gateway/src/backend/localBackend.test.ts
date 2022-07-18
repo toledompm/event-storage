4b3336dfc9dad07dc36eb5d08892125bd1e81479
toledompm <toledompm@gmail.com>
2022-07-17 22:32:07 -0300

    style: lint fixes

--- event-gateway/src/backend/localBackend.test.ts
+++ event-gateway/src/backend/localBackend.test.ts
@@ -1,16 +1,16 @@
 import test, { ExecutionContext } from 'ava';
 import { EventChunk } from 'src/proto/events_pb';
 
-import  { LocalBackend } from '../../src/backend/localBackend';
+import { LocalBackend } from '../../src/backend/localBackend';
 
-const setup = (): {backend: LocalBackend} => {
+const setup = (): { backend: LocalBackend } => {
   const backend = new LocalBackend('/tmp');
 
   return { backend };
-}
+};
 
 const persistsChunk = async (t: ExecutionContext, input: EventChunk) => {
   t.pass();
-}
+};
 
 test('persistChunk', persistsChunk, new EventChunk());
