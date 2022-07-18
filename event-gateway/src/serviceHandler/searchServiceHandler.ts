758d26e60737cf74617f4a12ac5d95bd987d1ae5
toledompm <toledompm@gmail.com>
2022-07-17 22:11:33 -0300

    style: lint fix

--- event-gateway/src/serviceHandler/searchServiceHandler.ts
+++ event-gateway/src/serviceHandler/searchServiceHandler.ts
@@ -1,9 +1,8 @@
 import { ServerWritableStream } from '@grpc/grpc-js';
-
-import { EventChunk, GetEventRequest } from '../proto/events_pb';
+import { EventChunk, GetEventRequest } from 'src/proto/events_pb';
 
 export const getEventChunk = (
-  call: ServerWritableStream<GetEventRequest, EventChunk>,
+  call: ServerWritableStream<GetEventRequest, EventChunk>
 ) => {
   // TODO get event chunk from backend
   call.write(new EventChunk());
