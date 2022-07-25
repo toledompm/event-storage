fdae72402cd28f8d65865e8f804718985d3c0713
toledompm <toledompm@gmail.com>
2022-07-24 23:01:30 -0300

    test: localBackend persist chunks

--- event-gateway/src/backend/localBackend.test.ts
+++ event-gateway/src/backend/localBackend.test.ts
@@ -1,16 +1,35 @@
 import test, { ExecutionContext } from 'ava';
-import { EventChunk } from 'src/proto/events_pb';
+import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
+import * as sinon from 'sinon';
+import { LocalBackend } from 'src/backend/localBackend';
+import { Event, EventChunk } from 'src/proto/events_pb';
 
-import { LocalBackend } from '../../src/backend/localBackend';
+const BACKEND_PATH = '/tmp';
 
-const setup = (): { backend: LocalBackend } => {
-  const backend = new LocalBackend('/tmp');
+const setup = (): { backend: LocalBackend; writeFileMock: sinon.SinonStub } => {
+  const writeFileMock = sinon.stub();
 
-  return { backend };
+  const backend = new LocalBackend(BACKEND_PATH, writeFileMock);
+
+  return { backend, writeFileMock };
 };
 
-const persistsChunk = async (t: ExecutionContext, input: EventChunk) => {
-  t.pass();
-};
+test('persistChunk', async (t: ExecutionContext) => {
+  const { backend, writeFileMock } = setup();
 
-test('persistChunk', persistsChunk, new EventChunk());
+  const event = new Event();
+  event.setTimestamp(new Timestamp());
+  event.setDescription('test');
+
+  const eventChunk = new EventChunk();
+  eventChunk.setEventsList([event]);
+
+  await backend.persistChunk(eventChunk);
+
+  const stringifiedEvents = JSON.stringify([event]);
+  const regexMatcher = sinon.match(new RegExp(`${BACKEND_PATH}/.+\\.json`));
+
+  t.assert(
+    writeFileMock.calledOnceWithExactly(regexMatcher, stringifiedEvents)
+  );
+});
